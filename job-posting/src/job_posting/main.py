5491dc8003504ec70bce2022356317bb05d59300
Fidelek84 <office@net4client.pl>
2024-10-07 12:57:57 +0200

    Updated .env files and Python project config

--- job-posting/src/job_posting/main.py
+++ job-posting/src/job_posting/main.py
@@ -1,13 +1,15 @@
 import sys
-from job_posting.crew import JobPostingCrew
+from .crew import JobPostingCrew
+#from job_posting.crew import JobPostingCrew
+
 
 def run():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
-        'company_domain':'careers.wbd.com',
-        'company_description': "Warner Bros. Discovery is a premier global media and entertainment company, offering audiences the world’s most differentiated and complete portfolio of content, brands and franchises across television, film, sports, news, streaming and gaming. We're home to the world’s best storytellers, creating world-class products for consumers",
-        'hiring_needs': 'Production Assistant, for a TV production set in Los Angeles in June 2025',
-        'specific_benefits':'Weekly Pay, Employee Meals, healthcare',
+        'company_domain':'https://atos.net/',
+        'company_description': "Atos is an IT company. It offering Network, security and cloud services and support for mmanny big customers. Lot of different skilled engineers, managers and atchitect are working there.",
+        'hiring_needs': 'There is new project for elektomonility company to migrate their DC to Azure cloud',
+        'specific_benefits':'Monthly Pay, ambicius project, healthcare, good working atmosphere',
     }
     JobPostingCrew().crew().kickoff(inputs=inputs)
 
@@ -18,10 +20,10 @@ def train():
     Train the crew for a given number of iterations.
     """
     inputs = {
-        'company_domain':'careers.wbd.com',
-        'company_description': "Warner Bros. Discovery is a premier global media and entertainment company, offering audiences the world’s most differentiated and complete portfolio of content, brands and franchises across television, film, sports, news, streaming and gaming. We're home to the world’s best storytellers, creating world-class products for consumers",
-        'hiring_needs': 'Production Assistant, for a TV production set in Los Angeles in June 2025',
-        'specific_benefits':'Weekly Pay, Employee Meals, healthcare',
+        'company_domain':'https://atos.net/',
+        'company_description': "Atos is an IT company. It offering Network, security and cloud services and support for mmanny big customers. Lot of different skilled engineers, managers and atchitect are working there.",
+        'hiring_needs': 'There is new project for elektomonility company to migrate their DC to Azure cloud',
+        'specific_benefits':'Monthly Pay, ambicius project, healthcare, good working atmosphere',
     }
     try:
         JobPostingCrew().crew().train(n_iterations=int(sys.argv[1]), inputs=inputs)
